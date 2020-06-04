2cdb09da9e728155d25b3ebd908f7a5d95c3f6bf
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-06-04 05:34:58 -0700

    Static URL

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -150,7 +150,7 @@ AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-STATIC_URL = 'http://' + skeeb + '.s3.amazonaws.com/'
+STATIC_URL = 'http://skeeb.s3-website.us-east-2.amazonaws.com'
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
