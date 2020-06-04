47f5d02416252009eb52e0f57824ba35a7ddcec8
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-06-04 05:20:55 -0700

    AWS4 Again

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -150,6 +150,7 @@ AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = 'http://' + skeeb + '.s3.amazonaws.com/'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
